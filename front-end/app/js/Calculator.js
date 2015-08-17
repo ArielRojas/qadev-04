fd5027d7cdc21294057e409c46791c948482b1af
ArielRojas <arielwagnerrojas@gmail.com>
2015-08-17 14:19:53 -0400

    push

---
+++ front-end/app/js/Calculator.js
@@ -0,0 +1,46 @@
+/**
+ * Created by ArielWagner on 17/08/2015.
+ */
+
+Calculator = function()
+{
+    var numbers = arguments;
+    var pos = numbers.length - 1;
+    this.sum = function(numbers, pos)
+    {
+        var res = numbers[pos];
+        if(pos != 0)
+        {
+            res = numbers[pos] + sum(numbers, pos - 1);
+        }
+        return res;
+    };
+    this.max = function(numbers, pos)
+    {
+        var res = numbers[pos];
+        if(pos != 0)
+        {
+            if(d[pos-1] > res)
+            {
+                res = max(numbers, pos - 1);
+            }
+        }
+        return res;
+    };
+    this.min = function(numbers, pos)
+    {
+        var res = numbers[pos];
+        if(pos != 0)
+        {
+            if(d[pos-1] < res)
+            {
+                res = min(numbers, pos - 1)
+            }
+        }
+        return res;
+    };
+    this.prom = function (numbers, pos) {
+        var res = sum(numbers, pos)/ numbers.length;
+        return res;
+    }
+};
---
+++ front-end/app/js/main.js
@@ -0,0 +1,53 @@
+/**
+ * Created by ArielWagner on 10/08/2015.
+ */
+console.log('This is a JS code from a JS File');
+
+/*
+*Documentation...
+ */
+var sayHello = function (name) {
+    var helloStr = 'Hello ' + name + '!';
+    console.log(helloStr);
+};
+
+sayHello('Ariel');
+
+/**
+ *
+ * @param name
+ * @param age
+ */
+
+person = function(name, age){
+    this.name = name;
+    this.age = age;
+    this.sayHello = function(name){
+        console.log(arguments);
+        console.log('Hello', name, ' my name is  ', this.name);
+    };
+};
+
+paco = new person('Paco', 12);
+hugo = new person('Hugo', 13);
+
+//var age = '';
+var calculateAge = function(bornYear){
+    age =  2015 - bornYear;
+    return age;
+}
+ /*
+  var age = '';
+  var calculateAge = function(bornYear){
+  var age =  2015 - bornYear;
+  return age;
+  */
+
+operations = function(){
+
+
+
+    this.max = function(){
+
+    };
+};
