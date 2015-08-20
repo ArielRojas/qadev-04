f2bbf978811edab8962e76eda5d9769123b541ed
ArielRojas <arielwagnerrojas@gmail.com>
2015-08-20 16:10:07 -0400

    subir

---
+++ front-end/app/js/dataTypes.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ArielWagner on 20/08/2015.
+ */
+
+var countWords = function(par){
+    var par = "hola como estas";
+    var result = cadena.split(' ');
+    return result;
+}
+
+console.log('Number of words', countWords(par));
