a3d69a5ed44ec869df762ff8c2b29ff38d1580d1
ArielRojas <arielwagnerrojas@gmail.com>
2015-08-27 22:19:47 -0400

    August 27

--- front-end/app/js/functions.js
+++ front-end/app/js/functions.js
@@ -30,3 +30,34 @@ doOperation(1, 2, function(){
         name = 'world';
     console.log('Anonymous : hello!', name);
 })('Ariel');
+
+var Person = function(name, age){
+    this.name = name;
+    var _age = age;
+    this.getAge = function()
+    {
+        return _age;
+    };
+
+    this.setAge = function(age)
+    {
+        _age = age;
+    };
+    var _doSomethingPrivate = function(){
+        console.log('This is a private method');
+    }
+};
+Person.prototype.printBio = function(){
+    console.log('Hello, my name is ', this.name, 'and I am', this.getAge(), 'years old');
+};
+
+//metodo estatico
+Person.sayHello = function(){
+    console.log('Say Hello');
+};
+//Test Person class
+var pepe = new Person('Pepe', 23);
+pepe.printBio();
+pepe._doSomethingPrivate(); // ERROR
+_doSomethingPrivate(); //ERROR
+Person.sayHello(); // metodo estatico
